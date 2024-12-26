8bcdbd10705368717c4b1054b2e60e6b17ace7d7
Aaditya Sharma <aadityasharma00445@gmail.com>
2024-12-26 16:21:38 +0530

    Project 1

---
+++ Background-Color-Switcher/script.js
@@ -0,0 +1,50 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+function change(color){
+  const head1 = document.querySelector('h1');
+  const head2 = document.querySelector('h2');
+  const head3 = document.querySelector('h3');
+  head1.style.color = color;
+  head2.style.color = color;
+  head3.style.color = color;
+}
+
+function wow(){
+  body.outerHTML = "<center><h1>Told You</h1></center>"
+}
+
+
+buttons.forEach(function(button){
+  button.addEventListener('click',function (e) {
+    console.log(e);
+    console.log(e.target);
+    if(e.target.id === 'grey'){
+      body.style.backgroundColor = e.target.id;
+      change("white");
+    }
+    if(e.target.id === 'white'){
+      body.style.backgroundColor = e.target.id;
+      change("black");
+    }
+    if(e.target.id === 'blue'){
+      body.style.backgroundColor = e.target.id;
+      change("orange");
+    }
+    if(e.target.id === 'yellow'){
+      body.style.backgroundColor = e.target.id;
+      change("red");
+    }
+    if(e.target.id === 'purple'){
+      body.style.backgroundColor = e.target.id;
+      change("white");
+    }
+    if(e.target.id === 'black'){
+      // body.style.backgroundColor = e.target.id;
+      // change("white")
+      wow()
+      
+      
+    }
+  })
+
+})
