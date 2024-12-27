5a87604f0dbc470d915e0af541b158863cf7bc3d
Aaditya Sharma <aadityasharma00445@gmail.com>
2024-12-27 20:13:49 +0530

    New Project 5

---
+++ Random Number Generator/script.js
@@ -0,0 +1,21 @@
+
+
+setInterval(function (){
+    const rand = parseInt((Math.random() * 100 )+ 1);
+    const randprint1 = document.querySelector('#randprint1');
+    randprint1.innerText = rand;
+} , 100)
+
+
+setInterval(function (){
+    const rand = parseInt((Math.random() * 100 )+ 1);
+    const randprint2 = document.querySelector('#randprint2');
+    randprint2.innerText = rand;
+} , 100)
+
+setInterval(function (){
+    const rand = parseInt((Math.random() * 100 )+ 1);
+    const randprint3= document.querySelector('#randprint3');
+    randprint3.innerText = rand;
+} , 100)
+
