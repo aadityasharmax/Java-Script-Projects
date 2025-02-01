80eb35cceacfa608b3d71f322d164a96377eaedb
Aaditya Sharma <aadityasharma00445@gmail.com>
2025-02-01 11:31:51 +0530

    changed api

--- Quotes-Generator/script.js
+++ Quotes-Generator/script.js
@@ -3,11 +3,11 @@ const quote = document.querySelector('.quote');
 const button = document.querySelector('.quote-generate-btn');
 
 button.addEventListener("click" , function(e) {
-    fetch("https://api.quotable.io/random")
+    fetch("https://dummyjson.com/quotes/random")
   .then(response => response.json())
   .then(data => {
       
-      quote.innerHTML = `${data.content} — ${data.author}`;
+      quote.innerHTML = `${data.quote} — ${data.author}`;
   })
   .catch(error => console.error("Error fetching quote:", error));
 
